844b46b786163ae400dfa6748a7c4df8ad4dcba6
Ewa <emiklewska@gmail.com>
2024-10-08 16:49:00 +0200

    table

--- csn_lab2/AIC_table.R
+++ csn_lab2/AIC_table.R
@@ -19,6 +19,22 @@ write_summary <- function(language, file) {
       "\\\\ \n")
 }
 
+write_summary_AIC <- function(language, file) {
+  degree_sequence = read.table(file, header = FALSE)
+  x <- degree_sequence$V1
+  result_geometric <- calculate_geometric(x)
+  result_zeta_gamma_2 <- calculate_zeta_gamma_2(x)
+  result_zeta <- calculate_zeta(x)
+  result_poisson <- calculate_poisson(x)
+  #result_zeta_right_truncated <- calculate_zeta_right_truncated(x)
+  
+  cat(language, "&", # language
+      result_poisson$AIC, "&", 
+      result_geometric$AIC, "&", 
+      result_zeta_gamma_2$AIC, "&", 
+      result_zeta$AIC, 
+      "\\\\ \n")
+}
 
 source = read.table("list_in.txt", 
                     header = TRUE,               # this is to indicate the first line of the file contains the names of the columns instead of the real data
@@ -31,3 +47,8 @@ for (x in 1:nrow(source)) {
   write_summary(source$language[x], source$file[x])
 }
 
+print("AIC \n\n")
+
+for (x in 1:nrow(source)) {
+  write_summary_AIC(source$language[x], source$file[x])
+}
